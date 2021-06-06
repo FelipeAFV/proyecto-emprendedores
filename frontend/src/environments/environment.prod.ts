e41dfd44ca54bf111bfc575aab83d0ef736658ed
Felipe Figueroa Vergara <felipeafigueroav@gmail.com>
2021-06-06 19:44:14 -0400

    commit for heroku

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
+
+const PORT = process.env.PORT;
 export const environment = {
   production: true,
-  ApiUrl: 'http://localhost:3000/api',
-  AuthUrl: 'http://localhost:3000'
+  ApiUrl: 'http://localhost:'+PORT+'/api',
+  AuthUrl: 'http://localhost:'+PORT
 };

5101f5f900a68d25bb4ab06e6f79457bb2260cb5
Felipe Figueroa Vergara <felipeafigueroav@gmail.com>
2021-06-07 11:06:43 -0400

    angular build

--- frontend/dist/proyecto-emprendedores-frontend/main.js
+++ frontend/dist/proyecto-emprendedores-frontend/main.js
@@ -160,8 +160,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
 const environment = {
     production: false,
-    ApiUrl: 'http://localhost:undefined/api',
-    AuthUrl: 'http://localhost:undefined'
+    ApiUrl: 'http://localhost/api',
+    AuthUrl: 'http://localhost'
 };
 
 
